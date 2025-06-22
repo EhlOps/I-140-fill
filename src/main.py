0798379ffae625ed51698d0fbcc820f4b284ec00
EhlOps <samtehlers@gmail.com>
2025-06-22 10:20:20 -0400

    POC

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import os
+import zipfile
+
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
@@ -12,9 +15,18 @@ async def chatbot_endpoint(user_input: str):
 
 
 @app.get("/download")
-async def download_endpoint():
+async def download_endpoint(user_input: str):
+    with open("/code/src/addition_information.json", "wb") as f:
+        f.write(user_input.encode("utf-8"))
+    with zipfile.ZipFile("/code/src/i-140.zip", "w") as zipf:
+        zipf.write("/code/src/i-140-filled.pdf", arcname="i-140-filled.pdf")
+        zipf.write(
+            "/code/src/addition_information.json", arcname="addition_information.json"
+        )
+    if os.path.exists("/code/src/addition_information.json"):
+        os.remove("/code/src/addition_information.json")
     return FileResponse(
-        "/code/src/i-140-filled.pdf",
-        media_type="application/pdf",
-        filename="i-140-filled.pdf",
+        "/code/src/i-140.zip",
+        media_type="application/zip",
+        filename="i-140.zip",
     )
